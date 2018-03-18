2626425d15b91bab93b6e65a43e81a5f2467e972
PawanKL <34525906+PawanKL@users.noreply.github.com>
2018-03-18 17:09:17 +0500

    loop

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,13 @@
+import java.util.*
 public class HelloWorld
 {
   public static void main(String args[])
   {
     System.out.println("Hello World");
     System.out.println("Id is K163965");
-    
+    for(int i=0;i<10;i++)
+    {
+        System.out.print("*t\n");
+    }
   }
 }
